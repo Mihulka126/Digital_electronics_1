cc4f4a9211723b209e8fb353fe15008e6700fc06
Mihulka126 <231676@vutbr.cz>
2023-02-21 12:42:59 +0100

    Create 03-vivado

---
+++ 03-vivado/multiplexer/multiplexer.srcs/sim_1/new/tb_mux_3bit_4to1.vhd
@@ -0,0 +1,65 @@
+------------------------------------------------------------
+--
+-- Testbench for 4-bit binary comparator.
+-- EDA Playground
+--
+-- Copyright (c) 2020 Tomas Fryza
+-- Dept. of Radio Electronics, Brno Univ. of Technology, Czechia
+-- This work is licensed under the terms of the MIT license.
+--
+------------------------------------------------------------
+
+library ieee;
+use ieee.std_logic_1164.all;
+
+------------------------------------------------------------
+-- Entity declaration for testbench
+------------------------------------------------------------
+entity tb_mux_3bit_4to1 is
+    -- Entity of testbench is always empty
+end entity tb_mux_3bit_4to1;
+
+------------------------------------------------------------
+-- Architecture body for testbench
+------------------------------------------------------------
+architecture testbench of tb_mux_3bit_4to1 is
+
+    -- Local signals
+    signal s_a           : std_logic_vector(3 - 1 downto 0);
+    signal s_b           : std_logic_vector(3 - 1 downto 0);
+    signal s_c           : std_logic_vector(3 - 1 downto 0);
+    signal s_d           : std_logic_vector(3 - 1 downto 0);
+    signal s_sel         : std_logic_vector(2 - 1 downto 0);
+    signal s_f_o         : std_logic_vector(3 - 1 downto 0);
+
+begin
+    -- Connecting testbench signals with comparator_2bit
+    -- entity (Unit Under Test)
+    uut_mux_3bit_4to1 : entity work.mux_3bit_4to1
+        port map(
+            a_i           => s_a,
+            b_i           => s_b,
+            c_i           => s_c,
+            d_i           => s_d,
+            sel_i         => s_sel,
+            f_o           => s_f_o
+        );
+
+    --------------------------------------------------------
+    -- Data generation process
+    --------------------------------------------------------
+    p_stimulus : process
+    begin
+        -- Report a note at the beginning of stimulus process
+        report "Stimulus process started" severity note;
+
+        -- First test case
+
+        -- If false, then report an error
+
+        -- Report a note at the end of stimulus process
+        report "Stimulus process finished" severity note;
+        wait;
+    end process p_stimulus;
+
+end architecture testbench;
